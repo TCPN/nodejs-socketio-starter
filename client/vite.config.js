c04b4e683800876445847d3d411fb5d622c37ab8
Yang-En Chen <boyenen@gmail.com>
2026-02-10 05:46:50 +0800

    chore: add csie workstations to allowedHosts of vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,18 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    allowedHosts: [
+      'ws1.csie.ntu.edu.tw',
+      'ws2.csie.ntu.edu.tw',
+      'ws3.csie.ntu.edu.tw',
+      'ws4.csie.ntu.edu.tw',
+      'ws5.csie.ntu.edu.tw',
+      'ws6.csie.ntu.edu.tw',
+      'ws7.csie.ntu.edu.tw',
+      'ws8.csie.ntu.edu.tw',
+      'ws9.csie.ntu.edu.tw',
+      'ws10.csie.ntu.edu.tw',
+    ],
+  },
 })
